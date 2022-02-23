cb398844fb189f89790a8c677967abc5db4b662d
jbcor <jbcor@LAPTOP-JCAT1KV6>
2022-02-23 18:33:46 +0100

    Metodo Modificado

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -36,13 +36,13 @@ public class Main {
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
-         try {
+       
+     try {
             System.out.println("Ingreso en cuenta");
             cuenta1.ingresar(1000);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
-   
         
     }
 }
